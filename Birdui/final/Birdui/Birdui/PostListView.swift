59a2adb98e8c3e46269a19598a246235b151c48c
Alex Lundquist <alex@alexlundquist.com>
2020-07-11 22:34:43 -0600

    Added my working Branch, so I can merge and push to develop

--- Birdui/final/Birdui/Birdui/PostListView.swift
+++ Birdui/final/Birdui/Birdui/PostListView.swift
@@ -7,12 +7,46 @@
 //
 
 import SwiftUI
-
+var post = PostViewModel()
 struct PostListView: View {
+  static let taskDateFormat: DateFormatter = {
+    let formatter = DateFormatter()
+    formatter.dateFormat = "d MMM, HH:mm"
+    return formatter
+  }()
+  let masscotImageSize: CGFloat = 50
+  let postImageSize: CGFloat = 100
+  
   var body: some View {
     // TODO: This should look exactly like the Birdie table view,
     // but with only one button.
-    Text("Layout header, new-post button, List of posts")
+    List(post.posts.indices){index in
+      VStack(alignment: .center){
+        VStack(alignment: .leading, spacing: 0.0){
+          HStack(alignment: .center){
+            Image("mascot_swift-badge")
+              .resizable()
+              .aspectRatio(contentMode: .fit)
+              .frame(width: self.masscotImageSize, height: self.masscotImageSize)
+            VStack(alignment: .leading){
+              Text(post.posts[index].userName)
+              Text("\(post.posts[index].timestamp, formatter: PostListView.taskDateFormat)")
+            }
+          }
+          Text("\(post.posts[index].textBody ?? "")")
+            .multilineTextAlignment(.leading)
+        }
+        
+        post.posts[index].uiImage.map{
+          Image(uiImage: $0)
+            .resizable()
+            .aspectRatio(contentMode: .fill)
+            .frame(width: self.postImageSize, height: self.postImageSize)
+          
+        }
+      }
+      
+    }
   }
 }
 
